383c87199a853a6bc238adf000dc2f8893d332fa
Leandro Frazao <leandro@golance.com>
2026-01-04 12:28:47 -0300

    fix dbt

--- dbt_project/models/stg_vendas.sql
+++ dbt_project/models/stg_vendas.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='view') }}
 
 SELECT
-    ROW_NUMBER() OVER (ORDER BY v.data_hora) as venda_id,
+    ROW_NUMBER() OVER (ORDER BY strptime(v.data_hora, '%d/%m/%Y %H:%M:%S')) as venda_id,
     REGEXP_REPLACE(CAST(v.telefone AS VARCHAR), '[^0-9]', '', 'g') as cliente_id,
-    v.data_hora::TIMESTAMP as data_venda,
+    strptime(v.data_hora, '%d/%m/%Y %H:%M:%S') as data_venda,
     LOWER(TRIM(v.servico)) as servico, 
     COALESCE(p.valor, 0.00) as valor_faturamento
 FROM raw_vendas v
